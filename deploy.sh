9571b9e7123ce4924d8a173fd364a6640def5b29
Artem <valera8461@gmail.com>
2022-05-03 14:29:55 +0300

    change public path in vue config for firebase hosting

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ cd dist
 
 git init
 git add -A
-git commit -m 'v1.1 theme changed'
+git commit -m 'v1.2 clock worked'
 
 git push -f git@github.com:artem-2237/VueWeather.git master:gh-pages
 
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/VueWeather/'
-    : '/',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/VueWeather/'
+  //   : '/',
   transpileDependencies: true,
   css: {
     loaderOptions: {
